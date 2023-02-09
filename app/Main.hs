29ddeec4ceaa80692607b8e36666b758fbef6cb9
RJ <90836237+HR-RJ@users.noreply.github.com>
2023-02-09 15:06:09 +0100

    opdr1a en 1b

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,16 @@
 module Main where
 
-import qualified MyLib (someFunc)
+-- opdracht 1a
+-- main :: IO ()
+-- fact :: (Eq t, Num t) => t -> t
+-- fact 0 = 1
+-- fact x = x * fact(x-1)
+-- main = print (fact 4)
 
+-- opdracht 1b
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+fact :: (Eq t, Num t, Ord t) => t -> t
+fact factorial
+  | factorial <= 0 = 1
+  | otherwise = factorial * fact(factorial-1)
+main = print (fact 5)
