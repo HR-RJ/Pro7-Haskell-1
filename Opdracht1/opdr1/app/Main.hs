43e0a22bf85599121773ee392e3c5af23e4073fe
RJ <90836237+HR-RJ@users.noreply.github.com>
2023-02-09 16:31:59 +0100

    fixed names

--- Opdracht1/opdr1/app/Main.hs
+++ Opdracht1/opdr1/app/Main.hs
@@ -3,14 +3,14 @@ module Main where
 main :: IO ()
 
 -- opdracht 1a
-facb :: (Eq t, Num t) => t -> t
-facb 0 = 1
-facb x = x * facb(x-1)
+faca :: (Eq t, Num t) => t -> t
+faca 0 = 1
+faca x = x * faca(x-1)
 
 
 -- opdracht 1b
-faca :: (Eq t, Num t, Ord t) => t -> t
-faca factorial
+facb :: (Eq t, Num t, Ord t) => t -> t
+facb factorial
   | factorial <= 0 = 1
   | otherwise = factorial * facb(factorial-1)
 main = print (faca 5,facb 5)
