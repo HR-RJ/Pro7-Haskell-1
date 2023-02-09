28a5b7499c95c31f51919eca60b85f47d4c2c996
IcyKoala <90623742+IcyKoala@users.noreply.github.com>
2023-02-09 16:35:56 +0100

    Opdr 2a(?) & b

--- Opdracht1/opdr2/app/Main.hs
+++ Opdracht1/opdr2/app/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+nulpuntena::Double->Double->Double->[Double]
+nulpuntena a b c
+    | d < 0 = [0]
+    | otherwise = [(-b+sqrt d)/(2*a),
+                    (-b-sqrt d)/(2*a)]
+
+    where d = (b^2 - 4*a*c)
+
+main = print(nulpuntena 1 8 6)
+
