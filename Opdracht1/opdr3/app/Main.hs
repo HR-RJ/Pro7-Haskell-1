cd4899d6162eeecca4361e2846e2249961859b0f
RJ <90836237+HR-RJ@users.noreply.github.com>
2023-02-22 15:31:57 +0100

    Co-authored-by: IcyKoala <IcyKoala@users.noreply.github.com>

--- Opdracht1/opdr3/app/Main.hs
+++ Opdracht1/opdr3/app/Main.hs
@@ -19,7 +19,7 @@ main :: IO ()
 --     else calculateResult (a', b', c')
 
 findResult:: () -> [(Double, Double, Double)]
-findResult () = [(a, b, c) | a <- [0..1000], b <- [0..1000], c <- [0..1000], a == 2*(b+c), b == a*c, c == (a+b)/2]
+findResult () = [(a, b, c) | a <- [(-100)..1000], b <- [(-100)..1000], c <- [(-100)..1000], a == 2*(b+c), b == a*c, c == (a+b)/2]
 
 main = print (findResult())
 
