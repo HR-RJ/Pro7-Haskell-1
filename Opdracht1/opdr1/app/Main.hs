3351358d66b66e30655c33bdfe08dca0ff9512db
RJ <90836237+HR-RJ@users.noreply.github.com>
2023-02-23 15:52:07 +0100

    changed to Int

--- Opdracht1/opdr1/app/Main.hs
+++ Opdracht1/opdr1/app/Main.hs
@@ -3,13 +3,13 @@ module Main where
 main :: IO ()
 
 -- opdracht 1a
-faca :: (Eq t, Num t) => t -> t
+faca :: Int -> Int
 faca 0 = 1
 faca x = x * faca(x-1)
 
 
 -- opdracht 1b
-facb :: (Eq t, Num t, Ord t) => t -> t
+facb :: Int -> Int
 facb factorial
   | factorial <= 0 = 1
   | otherwise = factorial * facb(factorial-1)
