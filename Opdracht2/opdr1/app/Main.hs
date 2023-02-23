63704886c50e82be24f6de03108c61a56e949516
RJ <90836237+HR-RJ@users.noreply.github.com>
2023-02-23 16:30:53 +0100

    ???

--- Opdracht2/opdr1/app/Main.hs
+++ Opdracht2/opdr1/app/Main.hs
@@ -18,16 +18,23 @@ euclid x y
   | otherwise = euclid x (y-x)
 
 --1b
-egcd :: Integer -> Integer -> (Integer,Integer,Integer)
+-- egcd :: Integer -> Integer -> (Integer,Integer,Integer)
+-- egcd 0 b = (b, 0, 1)
+-- egcd a b =
+--     let (g, s, t) = egcd (mod b a) a
+--     in (g, t - div b a * s, s)
+
+-- egcd a b
+--     | s >= 0 = (g, t - div b a * s, s)
+--     | otherwise = s + mod b a
+--       where (g, s, t) = egcd (mod b a) a
+
+egcd :: Integer -> Integer -> (Integer, Integer, Integer)
 egcd 0 b = (b, 0, 1)
 egcd a b =
-    let (g, s, t) = egcd (mod b a) a
-    in (g, t - div b a * s, s)
-
-egcd a b
-    | s >= 0 = (g, t - div b a * s, s)
-    | otherwise = s + mod b a
-      where (g, s, t) = egcd (mod b a) a
+    let (g, s, t) = egcd (b `mod` a) a
+        k = (s * a + t * b) `div` g
+    in (g, (t - (b `div` g) * s) `mod` (b `div` g), (s + (a `div` g) * k) `mod` (b `div` g))
 
 
-main = print ({-euclid 1024 4096-} egcd 10 5)
+main = print ({-euclid 1024 4096-} egcd 1235 5238)
