6ee618535456e9ce0f5bf4e89769c360d3830f18
RJ <90836237+HR-RJ@users.noreply.github.com>
2023-03-16 16:34:55 +0100

    added 3.2

--- Opdracht3/opdr1/app/Main.hs
+++ Opdracht3/opdr1/app/Main.hs
@@ -5,4 +5,7 @@ main :: IO ()
 differentieer::(Double->Double)->Double->Double->Double
 differentieer f p x = (f (x+p) - f x) / p
 
+integreer::(Double->Double)->Double->Double->Double->Double
+integreer f a b p = sum [f x * p | x <- [a, a+p..b]]
+
 main = print(differentieer (\x -> x^2) 0.0001 2) -- ??
---
+++ Opdracht3/opdr2/app/Main.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import Data.List
+
+main :: IO ()
+
+dubbelen::[a]->[a]
+dubbelen s 
+
+main = putStrLn "Hello, Haskell!"
