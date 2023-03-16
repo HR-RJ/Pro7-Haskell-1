1162062f9de80c746622344ddb56c83bd2afb5bc
RJ <90836237+HR-RJ@users.noreply.github.com>
2023-03-16 15:23:28 +0100

    Keygen?Pog

--- Opdracht2/opdr2/app/Main.hs
+++ Opdracht2/opdr2/app/Main.hs
@@ -5,26 +5,26 @@ main :: IO ()
 
 -- Given a and m, return Just x such that ax = 1 mod m.
 -- If there is no such x return Nothing.
-modInv :: Int -> Int -> Maybe Int
-modInv a m
-  | 1 == g = Just (mkPos i)
-  | otherwise = Nothing
-  where
-    (i, _, g) = gcdExt a m
-    mkPos x
-      | x < 0 = x + m
-      | otherwise = x
+-- modInv :: Integer -> Integer -> Maybe Integer
+-- modInv a m
+--   | 1 == g = Just (mkPos i)
+--   | otherwise = Nothing
+--   where
+--     (i, _, g) = gcdExt a m
+--     mkPos x
+--       | x < 0 = x + m
+--       | otherwise = x
 
--- Extended Euclidean algorithm.
--- Given non-negative a and b, return x, y and g
--- such that ax + by = g, where g = gcd(a,b).
--- Note that x or y may be negative.
-gcdExt :: Int -> Int -> (Int, Int, Int)
-gcdExt a 0 = (1, 0, a)
-gcdExt a b =
-  let (q, r) = a `quotRem` b
-      (s, t, g) = gcdExt b r
-  in (t, s - q * t, g)
+-- -- Extended Euclidean algorithm.
+-- -- Given non-negative a and b, return x, y and g
+-- -- such that ax + by = g, where g = gcd(a,b).
+-- -- Note that x or y may be negative.
+-- gcdExt :: Int -> Int -> (Int, Int, Int)
+-- gcdExt a 0 = (1, 0, a)
+-- gcdExt a b =
+--   let (q, r) = a `quotRem` b
+--       (s, t, g) = gcdExt b r
+--   in (t, s - q * t, g)
 
 keygen :: Integer -> Integer -> (Integer, Integer, Integer)
 keygen p q = (m, e, d)
@@ -32,7 +32,7 @@ keygen p q = (m, e, d)
         m = p * q
         phiM = (p - 1) * (q - 1)
         e = head [e | e <-[2..], e <= phiM && gcd e phiM == 1]
-        d = mod phiM 1 `div` e
+        d = head [d | d <-[2..], d <= phiM && gcd d phiM == 1, e * d `mod` phiM == 1]
 
--- main = print (keygen 7 13)
-main = mapM_ print [5 `modInv` 72, 42 `modInv` 2017]
+main = print (keygen 7 13)
+-- main = mapM_ print [5 `modInv` 72, 42 `modInv` 2017]
