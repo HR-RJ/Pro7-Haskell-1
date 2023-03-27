ab12d3063e63b0a5b9a9ce61a2b37b9b59276319
RJ <1035471@hr.nl>
2023-03-27 16:36:55 +0100

    added opdr3 and made opdr2 work

--- Opdracht3/opdr2/app/Main.hs
+++ Opdracht3/opdr2/app/Main.hs
@@ -6,20 +6,18 @@ import Data.List
 main :: IO ()
 
 dubbelen::Ord a => [a]->[a]
---dubbelen a = nubBy (\ a  b -> a == b)  (sort a)
-dubbelen a = ((>1).length) (group (sort a))
-[(group (sort a)) | ]
-
+dubbelen a = nub $ a \\ nub a
     
 
 main = do
-    print (dubbelen [1,2,2,4,5,6,2,7,8,9,10])
+    print (dubbelen [1,2,2,4,5,6,2,7,6,8,9,10])
+    print (dubbelen "aaafscvvsd")
 
--- elemIndices :: Eq a => a -> [a] -> [Int]
--- sort :: Ord a => [a] -> [a] 
--- deleteBy :: (a -> a -> Bool) -> a -> [a] -> [a] 
--- concat :: Foldable t => t [a] -> [a] 
--- groupBy (\a b -> a==b) sort a
--- length :: Foldable t => t a -> Int
+
+
+-- previous attempts
+--dubbelen a = nubBy (\ a  b -> a == b)  (sort a)
+-- dubbelen a = ((>1).length) (group (sort a))
+-- [(group (sort a)) | ]
 
 -- sort elemindices deleteby concat
