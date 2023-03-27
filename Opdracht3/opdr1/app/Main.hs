58b20fd5e40f73ac43dd83730d809d5b950c44e2
RJ <1035471@hr.nl>
2023-03-27 15:29:45 +0100

    opdracht 3 suffering
    Co-authored-by: IcyKoala <IcyKoala@users.noreply.github.com>

--- Opdracht3/opdr1/app/Main.hs
+++ Opdracht3/opdr1/app/Main.hs
@@ -8,4 +8,6 @@ differentieer f p x = (f (x+p) - f x) / p
 integreer::(Double->Double)->Double->Double->Double->Double
 integreer f a b p = sum [f x * p | x <- [a, a+p..b]]
 
-main = print(differentieer (\x -> x^2) 0.0001 2) -- ??
+main = do
+    print(differentieer (\x -> x^3 + 5*x) 0.00001 2.12) -- ??
+    print(integreer (\x -> x^3 + 5*x) 0.0 2.12 0.00001) -- ??
--- Opdracht3/opdr2/app/Main.hs
+++ Opdracht3/opdr2/app/Main.hs
@@ -2,9 +2,24 @@ module Main where
 
 import Data.List
 
+
 main :: IO ()
 
-dubbelen::[a]->[a]
-dubbelen s 
+dubbelen::Ord a => [a]->[a]
+--dubbelen a = nubBy (\ a  b -> a == b)  (sort a)
+dubbelen a = ((>1).length) (group (sort a))
+[(group (sort a)) | ]
 
-main = putStrLn "Hello, Haskell!"
+    
+
+main = do
+    print (dubbelen [1,2,2,4,5,6,2,7,8,9,10])
+
+-- elemIndices :: Eq a => a -> [a] -> [Int]
+-- sort :: Ord a => [a] -> [a] 
+-- deleteBy :: (a -> a -> Bool) -> a -> [a] -> [a] 
+-- concat :: Foldable t => t [a] -> [a] 
+-- groupBy (\a b -> a==b) sort a
+-- length :: Foldable t => t a -> Int
+
+-- sort elemindices deleteby concat
