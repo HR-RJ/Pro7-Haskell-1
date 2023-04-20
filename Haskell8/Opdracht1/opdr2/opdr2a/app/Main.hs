3c4e52fc864f4d4cbcb035d4bbbd16453845f9ab
RJ <90836237+HR-RJ@users.noreply.github.com>
2023-04-20 16:32:45 +0200

    fixed compressimage and added decompress

--- Haskell8/Opdracht1/opdr2/opdr2a/app/Main.hs
+++ Haskell8/Opdracht1/opdr2/opdr2a/app/Main.hs
@@ -4,32 +4,26 @@ import System.Environment
 import System.IO
 import Data.List
 import Data.String
-import Text.Printf (IsChar(fromChar))
+import Text.Printf (IsChar(fromChar, toChar))
 main :: IO ()
 
-tupleToString :: (Show a, Show b) => (a, b) -> String
-tupleToString (x, y) = show y  ++ show x
-
+tupleToString :: (Show a, Show b, IsChar a) => (a, b) -> String
+tupleToString (x, y) = show y ++ [toChar x]
 
 rlcompress:: [Char] -> [Char] -> IO ()
 rlcompress fileIn fileOut = do
     contents <- readFile fileIn
-    let chars = contents
-    let compressed = map (\x -> (head x, length x)) (group chars)
-    -- first (toInteger compressed)
-    -- let compressed = map (\x -> (head x, length x)) (group linesOfFile)
+    let compressed = map (\x -> (head x, length x)) (group contents)
     let res = concatMap tupleToString compressed
-    writeFile fileOut (filter (not . (`elem` "'")) res)
+    writeFile fileOut res
     
-arguments:: IO ()
-arguments = do
-    args <- getArgs
-    let fileIn = head args
-    let fileOut = last args
-    rlcompress fileIn fileOut
 
 main = do
-    arguments
-    print("Done")
+    putStrLn "Please enter the file to be compressed:"
+    input1 <- getLine
 
-        -- cabal run exes -- image.txt compress.txt
+    putStrLn "Please enter the output file to compress to:"
+    input2 <- getLine
+
+    rlcompress input1 input2
+    print("Done")
--- Haskell8/Opdracht1/opdr2/opdr2b/app/Main.hs
+++ Haskell8/Opdracht1/opdr2/opdr2b/app/Main.hs
@@ -1,4 +1,39 @@
 module Main where
-
+import Data.List
+import Text.Printf (IsChar(toChar))
+import Text.Read
+import Data.Char
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+
+-- splitPairs :: String -> [String]
+-- splitPairs [] = []
+-- splitPairs xs = let (pair, rest) = splitAt 2 xs
+--                 in pair : splitPairs rest
+
+parsePairs :: String -> [(Int, Char)]
+parsePairs [] = []
+parsePairs (x:xs) =
+  let (numStr, rest) = span isDigit (x:xs)
+      num = read numStr :: Int
+      (c:remaining) = rest
+  in (num, c) : parsePairs remaining
+
+rlDecompress :: [Char] -> [Char] -> IO ()
+rlDecompress fileIn fileOut = do
+    contents <- readFile fileIn
+    let interm = parsePairs contents
+    let res = (map (\(x,y) -> replicate x y) interm)
+    writeFile fileOut (concat res)
+
+main = do
+    putStrLn "Please enter the compressed file:"
+    input1 <- getLine
+
+    putStrLn "Please enter the output file:"
+    input2 <- getLine
+
+    rlDecompress input1 input2
+    -- arguments
+    print ("Done")
+
+-- cabal run exes -- compress.txt image.txt
