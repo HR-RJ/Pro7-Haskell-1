b0b282b1beca8883813736a2c638d788717b84ed
RJ <1035471@hr.nl>
2023-05-23 11:24:43 +0200

    opdracht 2: Finished :D
    Co-authored-by: IcyKoala <IcyKoala@users.noreply.github.com>

--- Haskell8/Opdracht2/opdr3/app/Main.hs
+++ Haskell8/Opdracht2/opdr3/app/Main.hs
@@ -2,24 +2,19 @@ module Main where
 
 import System.Environment
 import System.IO
--- import Data.List
-import Data.String
--- import Text.Printf (IsChar(fromChar, toChar))
--- import qualified GHC.IO.Device as Bintree
--- import qualified Text.Read as Bintree
-main :: IO ()
+import Data.String (IsString(fromString))
+import Data.Char
 
 data Bintree a = Empty
     | Branch  (Bintree a) a (Bintree a)
     deriving (Show, Eq, Ord, Read)
-    
+
 push :: (Ord a) => Bintree a -> a -> Bintree a
 push Empty a = Branch Empty a Empty
 push (Branch t1 v t2) val 
     | val == v = Branch t1 val t2
     | val > v = Branch t1 v (push t2 val)
     | val < v = Branch (push t1 val) v t2
-
 
 pushList :: (Ord a) => (Bintree a) -> [a] -> (Bintree a)
 pushList tree [] = tree
@@ -35,7 +30,6 @@ filterTree f (Branch t1 v t2)
     | f v = (filterTree f t1) ++ [v] ++ (filterTree f t2)
     | otherwise = (filterTree f t1) ++ (filterTree f t2)
 
-
 preorder :: (Bintree a) -> [a]
 preorder Empty = []
 preorder (Branch t1 v t2) = [v] ++ (preorder t1) ++ (preorder t2)
@@ -48,8 +42,8 @@ inorder :: (Bintree a) -> [a]
 inorder Empty = []
 inorder (Branch t1 v t2) = (inorder t1) ++ [v] ++ (inorder t2)
 
-    
 
+main :: IO ()
 main = do
     putStrLn "Please enter the file to be compressed:"
     fileInput <- getLine
@@ -57,13 +51,16 @@ main = do
     putStrLn "Please enter the output file to compress to:"
     fileOutput <- getLine
     contents <- readFile fileInput
-    print contents
-    print(pushList Empty contents)
-    writeFile fileOutput (show (mapTree (fromEnum) (pushList Empty contents)))
+    -- print contents
+    let tree = pushList Empty contents
+    writeFile fileOutput (show (mapTree (fromEnum) tree))
     var <- readFile fileOutput
-    read var :: Bintree Int
-    --(readFile fileOutput) >>= let variable
-
+    let bintree = read var :: Bintree Int
+    -- print bintree
+    let nextTree = mapTree chr bintree
+    -- print nextTree
+    print(inorder nextTree)
+    print(filterTree isDigit nextTree)
     print("done")
 
 
